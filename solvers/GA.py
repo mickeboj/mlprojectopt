d8710b6358a2181a7573df9ac447b50bc294308c
Mikael Nelsson <mikael.nelsson@live.se>
2017-05-04 16:09:16 +0200

    started implementing BFGS and some structre change

--- solvers/GA.py
+++ solvers/GA.py
@@ -8,12 +8,17 @@ class Genetic_Solver(object):
     def __init__(self):
         self.genome = None
         self.ga = None
+        self.freq = None
 
-    def set_fun(self,fun,dim):
+    def set_fun(self,fun,dim,freq):
         self.genome = G1DList.G1DList(dim)
         self.genome.evaluator.set(fun)
+        self.freq = freq
 
     def set_instance(self):
         self.ga = GSimpleGA.GSimpleGA(self.genome)
         self.ga.minimax = Consts.minimaxType["minimize"]
-    
+
+    def solve(self):
+        self.ga.evolve(freq_stats=self.freq)
+        return self.ga.bestIndividual().getRawScore()
