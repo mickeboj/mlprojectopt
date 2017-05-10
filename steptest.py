e39f6b0f553197cadebbf2e4b464a58dc8902fda
Emil Fleron <emil.fleron@gmail.com>
2017-05-10 10:55:12 +0200

    Added functionality to steptest

--- steptest.py
+++ steptest.py
@@ -1,15 +1,22 @@
-from solvers import GA
+from solvers import GA,BFGS
 from functions import step
 
-
+dim = 20
+freq = 20
 Gsolv = GA.Genetic_Solver()
-Gsolv.set_fun(step.obj_fun,20)
+Gsolv.set_fun(step.obj_fun,dim,freq)
 step.set_genome_par(Gsolv.genome,step.get_bounds())
 Gsolv.set_instance()
 step.set_ga_par(Gsolv.ga)
 
+Bsolv = BFGS.BFGS()
+Bsolv.set_dim(dim)
+Bsolv.set_obj_fun(step.obj_fun)
+Bsolv.set_opt(True)
 
 if __name__ == '__main__':
 
-    Gsolv.ga.evolve(freq_stats=10)
-    print "Best individual found was" ,Gsolv.ga.bestIndividual().getRawScore()
+    best = Gsolv.solve()
+    print "Best individual found for GA was" ,best
+    best2 = Bsolv.solve(step.get_bounds())
+    print "Best solution found using BFGS was", best2
